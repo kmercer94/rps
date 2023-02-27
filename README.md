2e247900f1b6152bcab3657a4609f45a1e83e78c
Kristy <kristy.mercer@gmail.com>
2023-02-27 17:27:56 -0500

    Learning git push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rps
 Rock Paper Scissors - Revisit
+
+Merging new branch file to Main.
---
+++ script.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const reloadbtn = document.getElementById("reloadbtn");
+
+function getComputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter){
+    if(letter === "r") return "Rock";
+    if(letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". You Win!";
+}
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = convertToWord(userChoice) + " loses to " + convertToWord(computerChoice) + ". You Lose.";
+}
+
+function draw(userChoice, computerChoice){
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = "DRAW!"; 
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+
+    if(userScore >= 5){
+        result_div.innerHTML = "END GAME! YOU WIN!!";
+        reloadbtn.style.visibility = "visible";
+    }
+
+    if(computerScore >= 5){
+        result_div.innerHTML = "END GAME! YOU LOSE!";
+        reloadbtn.style.visibility = "visible";
+    }}
+
+function main(){
+
+    reloadbtn.addEventListener('click', function() {
+        reloadButton();
+        })
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })
+    paper_div.addEventListener('click', function() {
+        game("p");
+    })
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    })
+}
+main();
+function reloadButton() { document.location.reload()};
